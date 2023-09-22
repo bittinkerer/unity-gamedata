8f14eba98e4a7903c7c3e76931d70ab2a97d20bd
bittinkerer <bittinkerer@gmail.com>
2023-09-21 21:35:05 -0700

    Expose ctor

--- Runtime/Models/GameDataVector2.cs
+++ Runtime/Models/GameDataVector2.cs
@@ -4,6 +4,16 @@ namespace Packages.Estenis.GameData_
 {
     public class GameDataVector2 : GameData<Vector2>
     {
-        
+        public GameDataVector2(Vector2 vector2) : base(vector2)
+        {
+            
+        }
+
+        public GameDataVector2() : base()
+        {
+            
+        }
+
+
     }
 }
